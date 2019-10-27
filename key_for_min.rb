1008733a7c026dcde3a73a34b4b33a4cecac6f98
Tavis Weeden <tavisweeden@gmail.com>
2019-10-27 01:44:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,9 @@
 
 #we need to iterate over the hash and find the key that has the smallest value, and return just that key. Else return nil.
 def key_for_min_value(name_hash)
- new_array = name_hash.to_a
- 
+  min_value = 0
+if
+
    
  if 
 else
